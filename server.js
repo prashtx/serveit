d8610a2a7166d3dc725045e94b5a529f7b1071d0
Prashant Singh <prashant@codeforamerica.org>
2012-05-21 19:09:57 -0700

    Fix relative path issues.
    Server paths that start with '/' are found relative to the current
    working directory.
    Server index.html by default.

--- server.js
+++ server.js
@@ -28,6 +28,15 @@ function sendFile(response, filename, type) {
 
 app.get(/(.*)/, function(req, response) {
   var path = req.params[0];
+
+  if (path[path.length - 1] === '/') {
+    path = path + 'index.html';
+  }
+
+  if (path[0] === '/') {
+    path = '.' + path;
+  }
+
   var index = path.lastIndexOf('.');
   var format = '';
   if (index > -1) {
